199e0772b0783a1b6287895eb2572431ecfedf5b
Ville <ville.halmes@gmail.com>
2023-05-05 14:44:42 +0300

    1st project setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
